18a89e2a83ef575177ac90b3d594ecdb547d5e1f
minzzan <69343830+minzzan@users.noreply.github.com>
2021-01-02 18:54:09 +0900

    Create 10101.cpp

---
+++ 10101.cpp
@@ -0,0 +1,48 @@
+// 1월 1일이라서 10101 
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+	int A; int B; int C;
+
+	cin >> A;
+	cin >> B;
+	cin >> C;
+
+	if (A == 0 || B == 0 || C == 0)
+	{
+		printf("Error");
+		return 0;
+	}
+
+	if (A == 60 && B == 60 && C == 60)
+	{
+		printf("Equilateral");
+		return 0;
+	}
+	if (A + B + C == 180)
+	{
+		if (A == B || B == C || A == C)
+		{
+			printf("Isosceles");
+			return 0;
+		}
+		else
+		{
+			printf("Scalene");
+			return 0;
+		}
+	}
+	else if(A+B+C != 180)
+	{
+		printf("Error");
+		return 0;
+	}
+
+	return 0;
+
+}
